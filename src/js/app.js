11d71efadc530667de3b56b136a1b768aec33f8a
tomcremer <tom.cremer@student.hepl.be>
2024-02-19 15:32:56 +0100

    progress bar update

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+
+window.onscroll = () => {
+  let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  let scrolled = (winScroll / height) * 100;
+  document.getElementById("Bar").style.width = `${scrolled}%`;
+};
+
